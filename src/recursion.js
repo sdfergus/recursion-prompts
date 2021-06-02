6f8bb93cb2d6264f7f05e2cfc1d2eab7c61f520b
Shandira <shandira.ferguson@gmail.com>
2021-06-02 10:46:05 -0700

    Range passed

--- src/recursion.js
+++ src/recursion.js
@@ -85,6 +85,24 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  var results = [];
+
+  if(x === y) {
+      return results;
+  }
+
+  //Cases where x > y. eg: range(7,2)).to.eql([6,5,4,3]);
+  if(x > y) {
+    if(x !== y+1) {
+      results.push(x-1);
+    }
+    return results.concat(range(x-1, y));
+  }
+
+  if(x !== y-1) {
+    results.push(x+1);
+  }
+  return results.concat(range(x+1, y));
 };
 
 // 7. Compute the exponent of a number.
@@ -93,6 +111,7 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+
 };
 
 // 8. Determine if a number is a power of two.
