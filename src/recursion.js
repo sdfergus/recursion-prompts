41b2f358191a2800cf81a2d4c5c395be308d20c8
Shandira <shandira.ferguson@gmail.com>
2021-06-02 11:31:25 -0700

    Compute exponent passed

--- src/recursion.js
+++ src/recursion.js
@@ -111,7 +111,22 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+  var result = 0;
 
+  if(exp === 0) {
+    return 1;
+  }
+
+  if(exp === 1) {
+    return base;
+  }
+
+  if(exp < 0) {
+    result = 1 / base * exponent(base, exp + 1);
+    return parseFloat(result.toFixed(5));
+  }
+
+  return result = base * exponent(base, exp - 1);
 };
 
 // 8. Determine if a number is a power of two.
